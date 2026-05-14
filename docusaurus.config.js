b2cd816ded4e9f32faa3e96e5d02519f5f65b10a
Vicente <vicegd@gmail.com>
2026-05-14 10:38:30 +0200

    ✨ feat(docusaurus): add Matomo tracking script to head tags
    - include Matomo tracking script for enhanced analytics
    - enable page view tracking and link tracking for better insights

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -109,6 +109,25 @@ const config = {
       },
     }),
 
+  headTags: [
+    {
+      tagName: 'script',
+      attributes: {},
+      innerHTML: `
+        var _paq = window._paq = window._paq || [];
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//stats.garcia.at/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '7']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      `,
+    },
+  ],
+
   plugins: [
     pluginSiteData,
     [
